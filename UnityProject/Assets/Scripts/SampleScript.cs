32fa7a39986c0a2edfc08a3d48e877350be35f46
KonH <konh@yandex.ru>
2019-01-31 20:31:44 +0700

    Setup Weaver and made one sample

---
+++ UnityProject/Assets/Scripts/SampleScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using Weaver;
+
+public class SampleScript : MonoBehaviour {
+
+	[OnChanged(nameof(OnMyPropertyChanged))]
+	public string MyProperty { get; private set; }
+	
+	[MethodTimer]
+	[ProfileSample]
+	void Start() {
+		gameObject.SetActive(false);
+		MyProperty = "newValue";
+	}
+
+	void OnMyPropertyChanged(string newValue) {
+		Debug.Log("Value changed to " + newValue);
+	}
+}
